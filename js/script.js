86b3a34caeddb30b848ca63ae309da87f96e1c8b
KARAN KUMAR <kkvmalhi@gmail.com>
2023-05-06 23:43:43 +0500

    section Home all set

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 let typed = new Typed(".multi-headline",{
-    // strings:["Frontend Developer","Gammer","Graphic Designer","Freelancer","B-Loger"],
-    strings:["Frontend Developer"],
+    strings:["Frontend Developer","Gammer","Graphic Designer","Freelancer","B-Loger"],
+    // strings:["Frontend Developer"],
     typeSpeed:80,
     backSpeed:80,
     backdelay:1000,
