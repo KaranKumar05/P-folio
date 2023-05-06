8e77af8a9690d486b180141cf6fc75d2e0a84990
KARAN KUMAR <kkvmalhi@gmail.com>
2023-05-06 23:10:24 +0500

    home section cmpt

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+let typed = new Typed(".multi-headline",{
+    // strings:["Frontend Developer","Gammer","Graphic Designer","Freelancer","B-Loger"],
+    strings:["Frontend Developer"],
+    typeSpeed:80,
+    backSpeed:80,
+    backdelay:1000,
+    loop:true
+})
